2e9aa06a5466dd97b80164dc2cb3a83ba03d9837
Emilio González <emi2k01@gmail.com>
2021-12-11 05:44:08 -0600

    Update file name and line numbers on file open

--- runtime/theme/public/main.js
+++ runtime/theme/public/main.js
@@ -1,24 +1,63 @@
 function main() {
     let fileButtons = document.getElementsByClassName("file-button");
-    let codeBlock = document.getElementById("code-block");
 
     for (let i = 0; i < fileButtons.length; i++) {
         const fileButton = fileButtons[i];
-        fileButton.addEventListener("click", function() {
+        fileButton.addEventListener("click", function (event) {
             let url = fileButton.getAttribute("data-url");
+            let fileName = event.currentTarget
+                .getElementsByClassName("file-name")[0]
+                .getAttribute("data-file-name");
 
             //TODO: Add error alert
             fetch(url)
-                .then(response => {
+                .then((response) => {
                     if (!response.ok) {
-                        throw new Error('request error');
+                        throw new Error("request error");
                     }
                     return response.text();
                 })
-                .then(code => codeBlock.innerHTML = code)
-                .catch(error => console.error('fetch error: ', error));
+                .then((code) => {
+                    updateCodeView(fileName, code);
+                })
+                .catch((error) => console.error("fetch error: ", error));
         });
     }
+}
+
+function updateCodeView(fileName, code) {
+    // - content
+    let codeBlock = document.getElementById("code-block");
+    codeBlock.innerHTML = code;
+
+    // - file name
+    document
+        .getElementsByClassName("file-name")[0]
+        .getElementsByTagName("span")[0].innerText = fileName;
+
+    // - line numbers
+    let lineNumbersHtml = "";
+    let line = 1;
+    for (let char of code) {
+        if (char == "\n") {
+            // is this way of constructing a string wrong?
+            // I doubt it matters since the average case should be in the hundreds of lines.
+            lineNumbersHtml += lineNumberTemplate(line);
+            line += 1;
+        }
+    }
+
+    // empty files look weird without any line number so at least add one.
+    if (lineNumbersHtml == "") {
+        lineNumbersHtml = lineNumberTemplate(1);
+    }
+
+    let lineNumbers = document.getElementsByClassName("line-numbers")[0];
+    lineNumbers.innerHTML = lineNumbersHtml;
+}
+
+function lineNumberTemplate(line) {
+    return `<a href=\"#\">${line}</a>\n`;
 }
 
 main();
