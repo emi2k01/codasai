3cf8ab9693e42ace11227dbea01f1acfdfc4746b
Emilio González <emi2k01@gmail.com>
2022-01-08 12:59:53 -0600

    Implement explorer menu

---
+++ runtime/theme/static/explorer.js
@@ -0,0 +1,15 @@
+export default function registerExplorers() {
+    registerOpeners();
+}
+
+function registerOpeners() {
+    let openers = document.querySelectorAll(".explorer-entries button");
+    for (let i = 0; i < openers.length; i++) {
+        const opener  = openers[i];
+
+        opener.addEventListener("click", () => {
+            let container = opener.parentNode;
+            container.classList.toggle("open");
+        })
+    }
+}
--- runtime/theme/static/main.js
+++ runtime/theme/static/main.js
@@ -1,8 +1,10 @@
 import registerOffscreens from "./offscreen.js";
+import registerExplorers from "./explorer.js";
 
 function main() {
     let offscreens = document.getElementsByClassName("offscreen");
     registerOffscreens(offscreens);
+    registerExplorers();
 }
 
 main();
