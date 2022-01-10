07f8ce78580889d7eec789c321b9bf168259ec71
Emilio González <emi2k01@gmail.com>
2022-01-10 01:13:33 -0600

    Change `highlight` action's behavior
    - Scroll the highlighted section into view
    - Start the `to` search after `from` match ending

--- runtime/theme/static/actions/highlight.js
+++ runtime/theme/static/actions/highlight.js
@@ -2,27 +2,39 @@ export default function highlight(from, to) {
     let regexFrom = new RegExp(from, "m");
     let regexTo = new RegExp(to, "m");
 
+    let codeEl = document.querySelector(".file-viewer code");
     let codeCopyEl = document.querySelector(".file-viewer code.copy");
-    let contents = codeCopyEl.innerText;
+    let contents = codeEl.innerText;
 
-    let idxFrom = contents.search(regexFrom);
-    if (idxFrom == -1) {
+    let fromResult = regexFrom.exec(contents);
+    if (fromResult == null) {
         return;
     }
 
-    // We start looking after `idxFrom` but then we convert the regex match
-    // index to absolute by adding `idxFrom + 1`
-    let contentsAfterIdxFrom = contents.substring(idxFrom + 1);
-    let relativeIdxTo = contentsAfterIdxFrom.search(regexTo);
-    let idxTo = relativeIdxTo + idxFrom + 1;
+    // We start looking after `offset` but then we convert the regex match
+    // index to absolute by adding `offset + 1`
+    let offset = fromResult.index + fromResult[0].length;
+    let contentsAfterFromIndex = contents.substring(offset);
+    let toResult = regexTo.exec(contentsAfterFromIndex);
+    if (toResult == null) {
+        return;
+    }
+    let toIndex = toResult.index + offset + 1;
 
-    let contentsHtmlHighlighted = escapeHtml(contents.substring(0, idxFrom));
+    let contentsHtmlHighlighted = escapeHtml(contents.substring(0, fromResult.index));
     contentsHtmlHighlighted += "<span class='highlight'>";
-    contentsHtmlHighlighted += escapeHtml(contents.substring(idxFrom, idxTo + 1));
+    contentsHtmlHighlighted += escapeHtml(contents.substring(fromResult.index, toIndex + toResult[0].length));
     contentsHtmlHighlighted += "</span>";
-    contentsHtmlHighlighted += escapeHtml(contents.substring(idxTo + 1));
+    contentsHtmlHighlighted += escapeHtml(contents.substring(toIndex + toResult[0].length));
 
     codeCopyEl.innerHTML = contentsHtmlHighlighted;
+
+    let highlightEl = codeCopyEl.querySelector(".highlight");
+    highlightEl.scrollIntoView({
+        behavior: "smooth",
+        inline: "nearest",
+        block: "center",
+    });
 }
 
 function escapeHtml(unsafe) {
